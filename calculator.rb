8de6927eab58a8bc3972802b0327e387e2da645b
Allister Driscoll <allisterdrisc@gmail.com>
2023-01-02 17:32:36 -0500

    add calculator after rubocop edits

--- calculator.rb
+++ calculator.rb
@@ -1,15 +1,15 @@
-#ask the user for two numbers
-#ask the user for an operation to perform
-#perform the operation on the two numbers 
-#output the result
+# ask the user for two numbers
+# ask the user for an operation to perform
+# perform the operation on the two numbers
+# output the result
 
 def prompt(message)
   Kernel.puts("==> #{message}")
-end 
+end
 
 def valid_number?(num)
   num.to_i != 0
-end 
+end
 
 def operation_to_message(op)
   case op
@@ -21,44 +21,44 @@ def operation_to_message(op)
     'Multiplying'
   when '4'
     'Dividing'
-  end 
+  end
 end
 
 prompt("Welcome to Calculator! Enter your name:")
-  
-  name = ''
-  loop do
-    name = Kernel.gets.chomp
-    
-    if name.empty?
-      prompt("Make sure to use a valid name.")
-    else 
-      break
-    end
-  end 
 
-loop do # main loop 
+name = ''
+loop do
+  name = Kernel.gets.chomp
+
+  if name.empty?
+    prompt("Make sure to use a valid name.")
+  else
+    break
+  end
+end
+
+loop do # main loop
   number1 = ''
   loop do
     prompt("What's the first number?")
     number1 = Kernel.gets.chomp
     if valid_number?(number1)
-      break 
-    else 
+      break
+    else
       prompt("Hmm...that doesn't look like a valid number")
-    end 
-  end 
+    end
+  end
 
   number2 = ''
-  loop do 
+  loop do
     prompt("What's the second number?")
     number2 = Kernel.gets.chomp
     if valid_number?(number2)
-      break 
-    else 
+      break
+    else
       prompt("Hmm...that doesn't look like a valid number")
-    end 
-  end 
+    end
+  end
   operator_prompt = <<-MSG
     What operation would you like to perform?
     1) add
@@ -67,37 +67,36 @@ loop do # main loop
     4)divide
   MSG
 
-  prompt(operator_prompt) 
-  
+  prompt(operator_prompt)
+
   operator = ''
-  loop do 
+  loop do
     operator = Kernel.gets.chomp
 
     if %w(1 2 3 4).include?(operator)
-      break 
-    else 
+      break
+    else
       prompt("Must choose 1, 2, 3, or 4")
-    end 
-  end 
+    end
+  end
 
   prompt("#{operation_to_message(operator)} the two numbers...")
-  result = case operator 
-            when '1' 
-              number1.to_i + number2.to_i
-            when '2'
-              number1.to_i - number2.to_i
-            when '3'
-              result = number1.to_i * number2.to_i
-            when '4'
-              result = number1.to_f / number2.to_f
-  end 
+  result = case operator
+           when '1'
+             number1.to_i + number2.to_i
+           when '2'
+             number1.to_i - number2.to_i
+           when '3'
+             number1.to_i * number2.to_i
+           when '4'
+             number1.to_f / number2.to_f
+           end
 
   prompt("The result is #{result}")
 
   prompt("Do you want to perform another operation?")
   answer = Kernel.gets.chomp
   break unless answer.downcase.start_with?('y')
-end 
+end
 
 prompt("Thank you for using the calculator. Good bye!")
-
