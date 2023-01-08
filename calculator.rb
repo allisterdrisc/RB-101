50a36dfaa67f9d380efe9d66d73a776f73eccc9b
Allister Driscoll <allisterdrisc@gmail.com>
2023-01-08 10:06:31 -0500

    add calculator pre bonus edits

--- calculator.rb
+++ calculator.rb
@@ -7,21 +7,30 @@ def prompt(message)
   Kernel.puts("==> #{message}")
 end
 
-def valid_number?(num)
-  num.to_i != 0
+def integer?(num)
+  num.to_i.to_s == num
 end
 
+def float?(num)
+  num.to_f.to_s == num 
+end 
+
+def valid_number?(num)
+  integer?(num) || float?(num)
+end 
+
 def operation_to_message(op)
-  case op
-  when '1'
-    'Adding'
-  when '2'
-    'Subtracting'
-  when '3'
-    'Multiplying'
-  when '4'
-    'Dividing'
-  end
+  word = case op
+         when '1'
+           'Adding'
+         when '2'
+           'Subtracting'
+         when '3'
+           'Multiplying'
+         when '4'
+           'Dividing'
+        end
+  word
 end
 
 prompt("Welcome to Calculator! Enter your name:")
